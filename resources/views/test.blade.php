a06decb86a36ef67ebc9980bd0d11b8f9727df2a
jose mateo carrasco <locutermo@gmail.com>
2018-10-19 06:02:59 -0500

    Corrigiendo

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PROBANDO</title>
+</head>
+<body>
+    <h1>ESTO ES UNA PRUEBA DE QUE SE HA SUBIDO CORRECTAMENTE EL PROYECTO</h1>
+</body>
+</html>
