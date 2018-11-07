38581c1567e073853ce4271bc65ad64d1682035e
jose mateo carrasco <locutermo@gmail.com>
2018-11-07 03:42:47 -0500

    Corrigiendo Semillas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,32 +12,51 @@ class UsersTableSeeder extends Seeder
      */
     public function run(){
     
-        // User::create([
-        //     'id'=>1,
-        //     'name' => 'Jose Francisco Mateo Carrasco',
-        //     'code' => 'jose.mc@admin.com',
-        //     'password' => bcrypt('admin'),
-        //     'type'=> 1,
-        // ]);
-        // User::create([
-        //     'id'=>2,
-        //     'name' => 'Juan Alexis Luque Ayala',
-        //     'code' => 'alexis.la@admin.com',
-        //     'password' => bcrypt('admin'),
-        //     'type'=> 3,
-        // ]);
-        // User::create([
-        //     'id'=>3,
-        //     'name' => 'Antoni Bancayan',
-        //     'code' => 'antoni.b@admin.com',
-        //     'password' => bcrypt('admin'),
-        //     'type'=> 2,
-        // ]);
         User::create([
             'id'=>1,
+            'name' => 'Jose Francisco',
+            'lastname'=> 'Mateo Carrasco',
+            'dni'=> '77041708',
+            'dateOfBirth'=> null,
+            'cellphone'=>'952283982',
+            'email'=> 'locutermo@gmail.com',
+            'office'=> 1,
+
+            'code' => 'jose.mc@admin.com',
+            'password' => bcrypt('admin'),
+        ]);
+        User::create([
+            'id'=>2,
+            'name' => 'Juan Alexis',
+            'lastname'=> 'Luque Ayala',
+            'dni'=> '98712312',
+            'dateOfBirth'=> null,
+            'cellphone'=>'09182312',
+            'email'=> 'luque@gmail.com',
+            'office'=> 3,
+
+            'code' => 'alexis.la@admin.com',
+            'password' => bcrypt('admin'),
+        ]);
+        User::create([
+            'id'=>3,
+            'name' => 'Antoni',
+            'lastname'=> 'Bancayan',
+            'dni'=> '77999282',
+            'dateOfBirth'=> null,
+            'cellphone'=>'099234987',
+            'email'=> 'antoni@gmail.com',
+            'office'=> 2,
+
+            'code' => 'antoni.b@admin.com',
+            'password' => bcrypt('admin'),
+        ]);
+
+        User::create([
+            'id'=>4,
             'name' => 'Anonimo',
             'lastname'=> 'Sin Apellido',
-            'dni'=> '77041708',
+            'dni'=> '11111092',
             'dateOfBirth'=> null,
             'cellphone'=>'987654321',
             'email'=> 'anonimo@gmail.com',
